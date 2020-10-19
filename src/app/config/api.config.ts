981554cbc05587042266e8b4492114506bd568c8
Ariel Lopes De Souza <ariel.edit@gmail.com>
2020-10-18 21:27:52 -0400

    tirando o localhost:8080 e aprontado para o HEROKU

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,7 @@
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8080'
+  baseUrl: 'https://myheroappweb.herokuapp.com'
 
   // http://localhost:8080
+  
+    //baseUrl: 'https://myheroappweb.herokuapp.com''
 };
