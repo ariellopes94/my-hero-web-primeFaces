49a69214a35c01f3f7f8350322ed4c9b636edd74
Ariel Lopes De Souza <ariel.edit@gmail.com>
2020-10-19 21:11:19 -0400

    testando rota

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  baseUrl: 'https://myheroappweb.herokuapp.com'
-  // baseUrl: 'http://localhost:8080'
+   baseUrl: 'https://myheroappweb.herokuapp.com'
+   // baseUrl: 'http://localhost:8080'
 
   // http://localhost:8080
   // baseUrl: 'https://myheroappweb.herokuapp.com''
