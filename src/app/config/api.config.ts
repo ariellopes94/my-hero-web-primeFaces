a6bb9d3318a06b92c1da99c69c4fb1dee913cd00
Ariel Lopes De Souza <ariel.edit@gmail.com>
2020-10-25 14:50:12 -0400

    aprontando para o heroku

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  //baseUrl : 'https://my-hero-web.herokuapp.com'
-   baseUrl: 'http://localhost:8080'
+  baseUrl : 'https://my-hero-web.herokuapp.com'
+  //  baseUrl: 'http://localhost:8080'
 
   // http://localhost:8080
   // baseUrl : 'https://my-hero-web.herokuapp.com'
