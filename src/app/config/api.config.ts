7dcc6b32be4049026092a6e41d1f242a50b41e42
Ariel Lopes De Souza <ariel.edit@gmail.com>
2021-01-13 00:10:09 -0400

    merge

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,16 +1,12 @@
 export const API_CONFIG = {
-<<<<<<< .mine
-  baseUrl : 'http://ec2-3-135-217-247.us-east-2.compute.amazonaws.com:8080'
+
   // baseUrl: 'http://localhost:8080'
-=======
+
    baseUrl : 'https://my-hero-web.herokuapp.com' 
    
->>>>>>> .theirs
-<<<<<<< .mine
 
-=======
    // baseUrl: 'http://localhost:8080'
->>>>>>> .theirs
+
 
   // http://localhost:8080
   // baseUrl : 'https://my-hero-web.herokuapp.com'
