63374e175133e872d19be7c593bf2422ea0c607b
Ariel Lopes De Souza <ariel.edit@gmail.com>
2021-01-13 15:10:36 -0400

    API LOCAL

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
-   baseUrl : 'https://my-hero-web.herokuapp.com' 
+   //baseUrl : 'https://my-hero-web.herokuapp.com' 
    
-   
+   baseUrl: 'http://localhost:8080'
    // baseUrl: 'http://localhost:8080'
 
   // http://localhost:8080
