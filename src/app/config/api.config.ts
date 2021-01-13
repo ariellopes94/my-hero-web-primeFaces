11b5db908000b8980aae2e76775626d68c43d735
Ariel Lopes De Souza <ariel.edit@gmail.com>
2021-01-12 23:59:13 -0400

    Netlify SPA ,evitando o erro 404

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,8 @@
 export const API_CONFIG = {
-   baseUrl : 'https://my-hero-web.herokuapp.com'   // baseUrl: 'http://localhost:8080'
+   baseUrl : 'https://my-hero-web.herokuapp.com' 
+   
+   
+   // baseUrl: 'http://localhost:8080'
 
   // http://localhost:8080
   // baseUrl : 'https://my-hero-web.herokuapp.com'
